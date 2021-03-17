2a2e673352db4395c423a828a71d2b1d6f0e5eca
Yu Xuan Wu <yuxu.wu1@gmail.com>
2021-03-16 21:20:50 -0400

    Printing error message from HRESULT

--- utils/WindowsTest.cpp
+++ utils/WindowsTest.cpp
@@ -2,7 +2,25 @@
 #include "WindowsTest.h"
 
 void WindowsTest::debug(UINT hr) {
+	LPTSTR errorText = NULL;
+
 	if(FAILED(hr)) {
 		std::cout << "Call failed" << std::endl;
+		FormatMessage(
+				FORMAT_MESSAGE_FROM_SYSTEM
+				|FORMAT_MESSAGE_ALLOCATE_BUFFER
+				|FORMAT_MESSAGE_IGNORE_INSERTS,
+				NULL,
+				hr,
+				0,
+				(LPTSTR)&errorText,
+				0,
+				NULL
+		);
+		if(NULL != errorText){
+			std::wcout << "Error text: " << errorText << std::endl;
+			LocalFree(errorText);
+			errorText = NULL;
+		}
 	}
 }
